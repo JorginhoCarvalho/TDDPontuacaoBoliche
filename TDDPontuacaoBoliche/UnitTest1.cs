b05a4f0a7ffe3a25fdbd5b196cbe7f34e67d6524
Jorge Carvalho <jorgecarvalhojljc@gmail.com>
2022-07-31 13:32:42 -0300

    Adicionando Projeto

---
+++ TDDPontuacaoBoliche/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Xunit;
+namespace TDDBoliche
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [MemberData(nameof(Rodadas))]
+        public void RodadasComTodosValoresZeros(string[][] rodadasBoliche)
+        {
+            Assert.Equal(0, PontuacaoTotal(rodadasBoliche));
+        }
+
+        public static IEnumerable<string[][]> Rodadas()
+        {
+            yield return new string[][] { new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" }, new[] { "0 0" } };
+        }
+
+        int PontuacaoTotal(string[][] rodadas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
