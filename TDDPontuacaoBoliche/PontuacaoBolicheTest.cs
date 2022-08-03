dbad075131f1bb39b0c6d1bf7edc6c0aa53b9111
Jorge Carvalho <jorgecarvalhojljc@gmail.com>
2022-08-03 16:31:08 -0300

    Passo - 4. Alterada função para contabilizar os spares.

--- TDDPontuacaoBoliche/PontuacaoBolicheTest.cs
+++ TDDPontuacaoBoliche/PontuacaoBolicheTest.cs
@@ -11,8 +11,15 @@ namespace TDDBoliche
         }
 
         [Theory]
-        [MemberData(nameof(RodadasComAPontuacaoDiferentesDeZero))]
-        public void PontuacaoTotalDeTodasAsRodadasComTodosValoresDiferentesDeZero(string[] rodadasBoliche, int pontuacaoTotalEsperada)
+        [MemberData(nameof(RodadasComJogadasDiferentesDeZero))]
+        public void PontuacaoTotalDeTodasAsRodadasComValoresDiferentesDeZero(string[] rodadasBoliche, int pontuacaoTotalEsperada)
+        {
+            Assert.Equal(pontuacaoTotalEsperada, PontuacaoTotal(rodadasBoliche));
+        }
+
+        [Theory]
+        [MemberData(nameof(RodadasComJogadasQueTemSpare))]
+        public void PontuacaoTotalDeTodasAsRodadasComSpare(string[] rodadasBoliche, int pontuacaoTotalEsperada)
         {
             Assert.Equal(pontuacaoTotalEsperada, PontuacaoTotal(rodadasBoliche));
         }
@@ -22,24 +29,39 @@ namespace TDDBoliche
             yield return new object[] { new string[] { "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0" }, 0 };
         }
 
-        public static IEnumerable<object[]> RodadasComAPontuacaoDiferentesDeZero()
+        public static IEnumerable<object[]> RodadasComJogadasDiferentesDeZero()
         {
             yield return new object[] { new string[] { "1 0", "2 0", "3 0", "4 0", "5 0", "6 0", "7 0", "8 0", "9 0", "0 0" }, 45 };
             yield return new object[] { new string[] { "0 0", "0 1", "0 3", "0 5", "0 7", "0 9", "0 8", "0 6", "0 4", "0 2" }, 45 };
             yield return new object[] { new string[] { "3 1", "2 4", "5 4", "8 1", "9 0", "1 5", "2 6", "3 6", "1 1", "2 5" }, 69};
         }
 
+        public static IEnumerable<object[]> RodadasComJogadasQueTemSpare()
+        {
+            yield return new object[] { new string[] { "1 /", "2 7", "3 0", "4 0", "5 0", "6 0", "7 0", "8 0", "9 0", "0 0" }, 63 };
+            yield return new object[] { new string[] { "0 /", "0 1", "0 3", "0 5", "0 7", "0 9", "0 8", "0 6", "0 4", "0 2" }, 55 };
+            yield return new object[] { new string[] { "3 1", "2 4", "5 4", "8 1", "9 0", "1 5", "2 6", "3 6", "1 1", "2 / 9" }, 90 };
+            yield return new object[] { new string[] { "3 1", "2 4", "5 4", "8 1", "9 0", "1 5", "2 6", "3 /", "1 1", "2 / 9" }, 92 };
+        }
+
         int PontuacaoTotal(string[] rodadas)
         {
             int pontuacaoTotal = 0;
-            foreach (var (rodada, numeroRodada) in rodadas.Select((value, i) => (value, i)))
+            int houveSpare = 1;
+
+            foreach (var (rodada, i) in rodadas.Select((rodada, i) => (rodada, i)))
             {
-                var valoresJogadas = rodada.Split(" ").Select(x => int.Parse(x)).ToList();
+                var valoresJogadas = rodada.Split(" ").Select(x => x).ToList();
 
-                var primeiraJogada = valoresJogadas[0];
-                var segundaJogada = valoresJogadas[1];
+                var primeiraJogada = int.Parse(valoresJogadas[0]);
+                var segundaJogada = valoresJogadas[1].Equals("/") ? 10 - primeiraJogada : int.Parse(valoresJogadas[1]);
 
-                pontuacaoTotal += primeiraJogada + segundaJogada;
+                pontuacaoTotal += primeiraJogada * houveSpare + segundaJogada;
+
+                houveSpare = valoresJogadas[1].Equals("/") ? 2 : 1;
+
+                if (i == 9 && valoresJogadas[1].Equals("/"))
+                    pontuacaoTotal += int.Parse(valoresJogadas[2]) * houveSpare;
             }
 
             return pontuacaoTotal;
