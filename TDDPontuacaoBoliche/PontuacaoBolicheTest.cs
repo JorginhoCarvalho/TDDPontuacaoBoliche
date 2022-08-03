1898f3ae6ad012c77c541dfefba10ddbeaccaf06
Jorge Carvalho <jorgecarvalhojljc@gmail.com>
2022-08-03 14:55:29 -0300

    Passo 3. Implementada função que retorna valor total da pontuação com valores das jogadas diferentes de zeros

--- TDDPontuacaoBoliche/PontuacaoBolicheTest.cs
+++ TDDPontuacaoBoliche/PontuacaoBolicheTest.cs
@@ -4,20 +4,45 @@ namespace TDDBoliche
     public class PontuacaoBolicheTest
     {
         [Theory]
-        [MemberData(nameof(Rodadas))]
-        public void RodadasComTodosValoresZeros(string[] rodadasBoliche)
+        [MemberData(nameof(RodadasComValoresZeros))]
+        public void PontuacaoTotalDeTodasAsRodadasComTodosValoresZeros(string[] rodadasBoliche, int pontuacaoTotalEsperada)
         {
-            Assert.Equal(0, PontuacaoTotal(rodadasBoliche));
+            Assert.Equal(pontuacaoTotalEsperada, PontuacaoTotal(rodadasBoliche));
         }
 
-        public static IEnumerable<object[]> Rodadas()
+        [Theory]
+        [MemberData(nameof(RodadasComAPontuacaoDiferentesDeZero))]
+        public void PontuacaoTotalDeTodasAsRodadasComTodosValoresDiferentesDeZero(string[] rodadasBoliche, int pontuacaoTotalEsperada)
         {
-            yield return new object[] { new string[] { "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0" } };
+            Assert.Equal(pontuacaoTotalEsperada, PontuacaoTotal(rodadasBoliche));
+        }
+
+        public static IEnumerable<object[]> RodadasComValoresZeros()
+        {
+            yield return new object[] { new string[] { "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0", "0 0" }, 0 };
+        }
+
+        public static IEnumerable<object[]> RodadasComAPontuacaoDiferentesDeZero()
+        {
+            yield return new object[] { new string[] { "1 0", "2 0", "3 0", "4 0", "5 0", "6 0", "7 0", "8 0", "9 0", "0 0" }, 45 };
+            yield return new object[] { new string[] { "0 0", "0 1", "0 3", "0 5", "0 7", "0 9", "0 8", "0 6", "0 4", "0 2" }, 45 };
+            yield return new object[] { new string[] { "3 1", "2 4", "5 4", "8 1", "9 0", "1 5", "2 6", "3 6", "1 1", "2 5" }, 69};
         }
 
         int PontuacaoTotal(string[] rodadas)
         {
-            return 0;
+            int pontuacaoTotal = 0;
+            foreach (var (rodada, numeroRodada) in rodadas.Select((value, i) => (value, i)))
+            {
+                var valoresJogadas = rodada.Split(" ").Select(x => int.Parse(x)).ToList();
+
+                var primeiraJogada = valoresJogadas[0];
+                var segundaJogada = valoresJogadas[1];
+
+                pontuacaoTotal += primeiraJogada + segundaJogada;
+            }
+
+            return pontuacaoTotal;
         }
     }
 }
